aff7ef33b3a5e6581fb6234e184fed642b78379e
knl.bhg@gmail.com <knl.bhg@gmail.com>
2024-08-18 21:46:41 +0530

    Add solution for https://leetcode.com/problems/product-of-array-except-self/description/?envType=study-plan-v2&envId=leetcode-75

---
+++ python/src/leetcode/product_of_array_except_self/solution.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Solution:
+    def productExceptSelf(self, nums: list[int]) -> list[int]:
+        pass
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4]
+    res = Solution().productExceptSelf(nums=nums)
+    print(res)
