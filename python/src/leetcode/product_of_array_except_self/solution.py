40fd94cb68cf191636ef4f752371f6e179bec9d0
knl.bhg@gmail.com <knl.bhg@gmail.com>
2024-08-20 09:53:28 +0530

    Add solution for https://leetcode.com/problems/product-of-array-except-self/

--- python/src/leetcode/product_of_array_except_self/solution.py
+++ python/src/leetcode/product_of_array_except_self/solution.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python3
+from functools import reduce
+
 
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        pass
+        def p(x: int, y: int):
+            return x * y
+
+        prefix_arr = reduce(lambda acc, x: acc + [(acc[-1] * x)], nums, [1])
+        del prefix_arr[-1]
+        
+        suffix_arr = reduce(lambda acc, x: acc + [(acc[-1] * x)], nums[::-1], [1])
+        del suffix_arr[-1]
+        suffix_arr = suffix_arr[::-1]
+
+        print(prefix_arr, suffix_arr)
+
+        def s(idx: int):
+            return prefix_arr[idx] * suffix_arr[idx]
+
+        return [s(idx) for idx, _ in enumerate(nums)]
 
 
 if __name__ == "__main__":
-    nums = [1, 2, 3, 4]
-    res = Solution().productExceptSelf(nums=nums)
+    from .inputs import input
+    res = Solution().productExceptSelf(nums=input)
     print(res)
