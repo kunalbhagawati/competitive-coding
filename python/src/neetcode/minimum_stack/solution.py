5a6b63d2e6a03fc32e74b4e65be2e678285cb5c2
Kunal Bhagawati <knl.bhg@gmail.com>
2025-05-03 11:31:07 +0530

    Complete https://neetcode.io/problems/minimum-stack (#22)

---
+++ python/src/neetcode/minimum_stack/solution.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# https://neetcode.io/problems/minimum-stack
+
+
+
+class MinStack:
+
+    def __init__(self):
+        self._stack = []
+        self._min_stack = []
+
+    def push(self, val: int) -> None:
+        self._stack.append(val)
+        try:
+            m = min(val, self._min_stack[-1])
+        except IndexError:
+            m = val
+        self._min_stack.append(m)
+
+    def pop(self) -> None:
+        self._stack.pop()
+        self._min_stack.pop()
+
+    def top(self) -> int:
+        return self._stack[-1]
+
+    def getMin(self) -> int:
+        return self._min_stack[-1]
+    
+    def __repr__(self):
+        return f"""
+STACK: {self._stack}
+MIN: {self._min_stack}
+"""
