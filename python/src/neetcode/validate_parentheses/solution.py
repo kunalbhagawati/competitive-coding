ebc78c9a15e1a10c68f2bcb2e7ae857fa1deccc4
knl.bhg@gmail.com <knl.bhg@gmail.com>
2025-05-03 10:38:43 +0530

    Complete https://neetcode.io/problems/validate-parentheses

---
+++ python/src/neetcode/validate_parentheses/solution.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from collections import deque
+
+
+# https://neetcode.io/problems/validate-parentheses
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        m = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+        
+        dq = deque()
+        for c in s:
+            if c in ['(', '{', '[']:
+                dq.append(c)
+            else:
+                try:
+                    el = dq.pop()
+                    if m[c] != el:
+                        return False
+                except IndexError:
+                    return False
+                    
+        return len(dq) == 0
+
+
+if __name__ == '__main__':
+    from python.src.neetcode.validate_parentheses.input import S
+    
+    res = Solution().isValid(S)
+    
+    print(res)
